29c337fb9e6a18a2c792f2c7379143ab07f04880
Love Zambre <zambrelove95@gmail.com>
2023-06-22 15:32:10 +0530

    second commit

---
+++ Assests/main.js
@@ -0,0 +1,10 @@
+function OpenSidebar(){
+    var element = document.getElementById('Sidebar');
+    element.classList.add('Active-Sidebar');
+}
+
+// Close SideBar
+function CloseSidebar(){
+    var element = document.getElementById('Sidebar');
+    element.classList.remove('Active-Sidebar');
+}
